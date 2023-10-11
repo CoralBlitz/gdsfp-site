aaa5a984470750dfd22eb51cbb364a9b9bbd7036
CoralBlitz <82882196+CoralBlitz@users.noreply.github.com>
2023-10-11 21:30:19 +0300

    based

---
+++ register.js
@@ -0,0 +1,4 @@
+let name = document.querySelector('#name')
+let login = document.querySelector('#login')
+let password = document.querySelector('#password')
+let submit = document.querySelector('#submit')
